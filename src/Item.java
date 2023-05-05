8b062930fc9b6bc57211ba3a5aed3463247c2dcf
Frederick Morrison <frederick.morrison@outlook.com>
2023-05-04 20:11:06 -0600

    update Item class

--- src/Item.java
+++ src/Item.java
@@ -3,4 +3,14 @@ public class Item
     String SKU;
     String name;
     PricingMethod pricingMethod;
+    double unitPrice;
+
+    public Item(String name, String SKU, PricingMethod pricingMethod, double unitPrice){
+        this.name = name;
+        this.SKU = SKU;
+        this.pricingMethod = pricingMethod;
+        this.unitPrice = unitPrice;
+    }
+
+    private Item() {/* prevent uninitialized instances */}
 }
